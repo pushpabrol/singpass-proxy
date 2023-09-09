e0008a2340f0d75bc58cee1be0490ad219a3b53a
pushpabrol <pushp.abrol@gmail.com>
2023-09-09 19:12:07 -0400

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { JWK, SignJWT, parseJwk } = require('node-jose');
+const { JWK } = require('node-jose');
+const {SignJWT, importJWK } = require('jose');
 const {Issuer, generators } = require('openid-client');
 const axios = require('axios');
 const jwt = require('jsonwebtoken');
 const uuid = require('uuid');
 const dotenv  = require('dotenv');
+const qs = require('querystring');
+const jwksClient = require('jwks-rsa');
+
 dotenv.config()
 
 const app = express();
@@ -13,44 +17,33 @@ const port = 3000;
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 
 // Create a route for the /authorize endpoint
 app.get('/authorize', async (req, res) => {
   // Generate code_verifier and code_challenge
   const code_verifier = generators.codeVerifier();
+  console.log("code_verifier:", code_verifier);
   const code_challenge = generators.codeChallenge(code_verifier);
 
   // Store code_verifier in Vercel KV store
   const { kv } = require("@vercel/kv");
   const nonce = generators.nonce();
   await kv.set(`${code_verifier}:nonce`, nonce);
+  //const nonce="12345";
 
-    var keyJSON = {
-        "kty": "EC",
-        "use": "sig",
-        "crv": "P-256",
-        "kid": "sig-2021-08-30T04:38:19Z",
-        "x": "pQ6PL6JX14NpKka8j261yHMYsCQ6t3YMlDLpwIIxYcI",
-        "y": "NHYjUfuLspMW_-5PaulnvkRd34vYeVogpl-WFv8tRkc",
-        "alg": "ES256",
-        "d" : "2FwqOVMxvumPcB4-wI0ZkTHnUYQcoj4TGwf3Ulq7E6s"
-    };
-    
-    
-    var key = await JWK.asKey(keyJSON)
-    var keystore = JWK.createKeyStore();
-    await keystore.add(key);
-
-    const auth0Issuer = await Issuer.discover(`https://${process.env.DOMAIN}`);
+   
+    const auth0Issuer = await Issuer.discover(`https://${process.env.IDP_DOMAIN}`);
 
     //console.log('Discovered issuer %s %O', auth0Issuer.issuer, auth0Issuer.metadata);
 
     const client = new auth0Issuer.Client({
-        client_id: process.env.SINGPASS_CLIENT_ID,
+        client_id: process.env.IDP_CLIENT_ID,
         token_endpoint_auth_method: 'private_key_jwt',
         redirect_uris: [process.env.RP_REDIRECT_URI]
 
-    },keystore.toJSON(true));
+    });
 
     const url = client.authorizationUrl({
         scope: `openid`,
@@ -69,27 +62,28 @@ app.get('/authorize', async (req, res) => {
 
 // Create a route for the /token endpoint
 app.post('/token', async (req, res) => {
+    console.log(req.body);
   // Retrieve sessionId from the query parameters
   const { client_id, code, code_verifier, redirect_uri } = req.body;
-
-
+  const auth0Issuer = await Issuer.discover(`https://${process.env.IDP_DOMAIN}`);
   const nonce = await kv.get(`${code_verifier}:nonce`);
+  //const nonce = "12345";
 
   if (!client_id) {
-    return res.status(400).send('Missing client_id / client_secret');
+    return res.status(400).send('Missing client_id');
   }
 
-  if (process.env.SINGPASS_CLIENT_ID === client_id) {
+  if (process.env.IDP_CLIENT_ID === client_id) {
     try {
       const client_assertion = await generatePrivateKeyJWT(process.env);
-
+      console.log(client_assertion);
       const options = {
         method: 'POST',
-        url: `https://${process.env.DOMAIN}/token`,
+        url: auth0Issuer.token_endpoint,
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         data: qs.stringify({
           grant_type: 'authorization_code',
-          client_id: process.env.SINGPASS_CLIENT_ID,
+          client_id: process.env.IDP_CLIENT_ID,
           client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
           client_assertion,
           code,
@@ -100,16 +94,18 @@ app.post('/token', async (req, res) => {
 
       const response = await axios.request(options);
       //return res.status(200).send(response.data);
-       const { id_token } = response.data;
-      const publicKey = await loadPublicKey(process.env);
+      console.log(response.data); 
+      const { id_token } = response.data;
 
+      const publicKeyIDP = await loadPublicKeyIDP(process.env);
     
        console.log(`nonce expected: ${nonce}`);
 
-       const { payload, protectedHeader } = await jwt.verify(id_token, publicKey, {
-         issuer: `https://${process.env.DOMAIN}/`,
-         audience: process.env.SINGPASS_CLIENT_ID,
+       const payload = await jwt.verify(id_token, publicKeyIDP, {
+         issuer: `https://${process.env.IDP_DOMAIN}`,
+         audience: process.env.IDP_CLIENT_ID,
        });
+       console.log(payload)
 
        if (payload.nonce !== nonce) {
          return res.status(400).send('Nonce mismatch');
@@ -118,6 +114,7 @@ app.post('/token', async (req, res) => {
          return res.status(200).send(response.data);
        }
     } catch (error) {
+        //console.error(error);
       if (error.response) {
         return res.status(error.response.status).send(error.response.data);
       } else {
@@ -132,26 +129,17 @@ app.post('/token', async (req, res) => {
 });
 
 // Create a route for /.well-known/jwks.json
-app.get('/.well-known/jwks.json', (req, res) => {
+app.get('/.well-known/keys', async (req, res) => {
   // Create and return a JSON Web Key Set (JWKS)
   const jwks = {
     keys: [JWK.asKey({ kty: 'RSA', use: 'sig' })],
   };
 
-  //res.json(jwks);
-  res.json({
-    "keys": [
-    {
-    "kty": "EC",
-    "use": "sig",
-    "crv": "P-256",
-    "kid": "sig-2021-08-30T04:38:19Z",
-    "x": "pQ6PL6JX14NpKka8j261yHMYsCQ6t3YMlDLpwIIxYcI",
-    "y": "NHYjUfuLspMW_-5PaulnvkRd34vYeVogpl-WFv8tRkc",
-    "alg": "ES256"
-    }
-    ]
-    });
+  var publicKey =  process.env.RELYING_PARTY_PUBLIC_KEY.replace(/\n/g,"\r\n");
+  var keystore = JWK.createKeyStore();
+  //var key2 = await JWK.asKey(privateKey,"pem");     
+   await keystore.add(publicKey, "pem");
+  res.json(keystore.toJSON());
 });
 
 // Start the Express server
@@ -161,32 +149,44 @@ app.listen(port, () => {
 
 
 async function loadPrivateKey() {
+    const baseUrl = getBaseUrl();
     try {
-      //const response = await axios.get(config.RELYING_PARTY_JWKS_ENDPOINT);
-      //const { keys } = response.data;
-      const keys = [
-          {
-          "kty": "EC",
-          "use": "sig",
-          "crv": "P-256",
-          "kid": "sig-2021-08-30T04:38:19Z",
-          "x": "pQ6PL6JX14NpKka8j261yHMYsCQ6t3YMlDLpwIIxYcI",
-          "y": "NHYjUfuLspMW_-5PaulnvkRd34vYeVogpl-WFv8tRkc",
-          "alg": "ES256"
-          }
-          ];
-      keys[0].d = process.env.RELYING_PARTY_PRIVATE_KEY;
-      return await parseJwk(keys[0], process.env.SINGPASS_SIGNING_ALG);
+        const response = await axios.get(`${baseUrl}/.well-known/keys`);
+        const { keys } = response.data;
+        keys[0].d = process.env.RELYING_PARTY_PRIVATE_KEY;
+        return await importJWK(keys[0], process.env.IDP_SIGNING_ALG);
     } catch (e) {
       return e;
     }
   }
   
   async function loadPublicKey() {
+
+    const baseUrl = getBaseUrl();
     try {
-      const response = await axios.get(`https://${process.env.DOMAIN}/.well-known/keys`);
-      const publicKey = await parseJwk(response.data.keys[0], process.env.SINGPASS_SIGNING_ALG);
+      const response = await axios.get(`${baseUrl}/.well-known/keys`);
+      console.log(response.data);
+      const publicKey = await importJWK(response.data.keys[0], process.env.IDP_SIGNING_ALG);
+      console.log(publicKey);
       return publicKey;
+    } catch (e) {
+      return e;
+    }
+  }
+
+  async function loadPublicKeyIDP() {
+    try {
+
+        const client = jwksClient({
+        jwksUri: `https://${process.env.IDP_DOMAIN}/jwks`,
+        requestHeaders: {}, // Optional
+        timeout: 30000 // Defaults to 30s
+        });
+        const kid = 'MFZeG102dQiqbANoaMlW_Jmf7fOZmtRsHt77JFhTpF0';
+        const key = await client.getSigningKey();
+        console.log(key);
+        const signingKey = key.publicKey || key.rsaPublicKey;
+    return signingKey;
     } catch (e) {
       return e;
     }
@@ -195,17 +195,29 @@ async function loadPrivateKey() {
   async function generatePrivateKeyJWT() {
     try {
       const key = await loadPrivateKey();
+      console.log(key);
       const jwt = await new SignJWT({})
-        .setProtectedHeader({ alg: process.env.SINGPASS_SIGNING_ALG, kid: process.env.RELYING_PARTY_KID, typ: "JWT" })
+        .setProtectedHeader({ alg: process.env.IDP_SIGNING_ALG, kid: process.env.RELYING_PARTY_KID, typ: "JWT" })
         .setIssuedAt()
-        .setIssuer(process.env.SINGPASS_CLIENT_ID)
-        .setSubject(process.env.SINGPASS_CLIENT_ID)
-        .setAudience(`https://${process.env.DOMAIN}/`)
+        .setIssuer(process.env.IDP_CLIENT_ID)
+        .setSubject(process.env.IDP_CLIENT_ID)
+        .setAudience([`https://${process.env.IDP_DOMAIN}/`, `https://${process.env.IDP_DOMAIN}/token`])
         .setExpirationTime('2m') // Expiration time
         .setJti(uuid.v4())
         .sign(key);
+        console.log(jwt);
+
       return jwt;
     } catch (error) {
+      console.log(error);
       return error;
     }
   }
+
+  const getBaseUrl = () => {
+    if (process.env.NODE_ENV === "development") {
+      return `${process.env.DEV_RP_URL}`
+    }
+    
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  };
